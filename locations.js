2d98b3a798d3a6a293419139119e3900535383e1
Karin Meijvogel <karinmeijvogel@hotmail.com>
2023-01-30 18:18:10 +0100

    Locations and routes in separate file

---
+++ locations.js
@@ -0,0 +1,42 @@
+const locations = [
+  {
+    title: "Varen met Sil",
+    location: [53.31791187972894, 6.169128705034792],
+    popup: {
+      description:
+        "Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.",
+      website: "https://google.com",
+      img: "https://www.rd.com/wp-content/uploads/2021/03/GettyImages-1133605325-scaled-e1617227898456.jpg",
+      facilities: {
+        price: "1,50",
+        toilet: "Aanwezig",
+        shower: "Aanwezig",
+        tel: "06123781294",
+        store: {
+          name: "Jumbo Dokkum",
+          url: "https://jumbo.nl",
+        },
+      },
+    },
+  },
+  {
+    title: "Varing met Karin",
+    location: [53.26791187972894, 6.069128705034792],
+    popup: {
+      description:
+        "Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.",
+      website: "https://google.com",
+      img: "https://www.rd.com/wp-content/uploads/2021/03/GettyImages-1133605325-scaled-e1617227898456.jpg",
+      facilities: {
+        price: "1,50",
+        toilet: "pissen in de bosjes",
+        shower: "Aanwezig",
+        tel: "06123781294",
+        store: {
+          name: "Jumbo Dokkum",
+          url: "https://jumbo.nl",
+        },
+      },
+    },
+  },
+];
